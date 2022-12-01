756578d961819c8fa94c9e08de07be42d14fc050
Carl Meyer <carl@ukiyo-studio.co.uk>
2022-12-01 23:48:35 +0000

    Input Helper

--- AdventCode2022/InputHelper.cs
+++ AdventCode2022/InputHelper.cs
@@ -1,6 +1,34 @@
-﻿namespace AdventCode2022;
+﻿using System.Text;
+
+namespace AdventCode2022;
 
 public class InputHelper
 {
     
+    public static StreamReader ConvertStringToStream(string input) => new(new MemoryStream(Encoding.UTF8.GetBytes(input)));
+
+    public static async Task<StreamReader> GetInput(int day)
+    {
+        string file = $"day{day}.txt";
+
+        if (!File.Exists(file))
+        {
+            string input = await GetInputFromUrl(day);
+            using StreamWriter writer = new(file);
+            writer.Write(input);
+        }
+
+        return new StreamReader(file);
+    }
+
+    private static async Task<string> GetInputFromUrl(int day)
+    {
+        string sessionCookie = File.ReadAllText("cookie.txt");
+
+        var client = new HttpClient();
+        HttpRequestMessage request = new(HttpMethod.Get, $"https://adventofcode.com/2022/day/{day}/input");
+        request.Headers.TryAddWithoutValidation("Cookie", $"session={sessionCookie}");
+        var response = await client.SendAsync(request);
+        return await response.Content.ReadAsStringAsync();
+    }
 }
